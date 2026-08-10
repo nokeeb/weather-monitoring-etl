aa77300a93b16ee9518273c458ca82fce49af95d
Kenan Baralic <kenann.bara@gmail.com>
2026-08-10 12:23:27 +0200

    add configuration and database connection helpers

--- scripts/create_database.py
+++ scripts/create_database.py
@@ -1,29 +1,19 @@
-from dotenv import load_dotenv
 import psycopg2
 from pathlib import Path
 import os
+PROJECT_ROOT=Path(__file__).resolve().parent.parent
+import sys
+sys.path.insert(0,str(PROJECT_ROOT))
+from src import config,database
 
 def main():
-    BASE_DIR=Path(__file__).resolve().parent.parent
-    load_dotenv(BASE_DIR/'.env')
     admin_conn=None
     admin_cur=None
     conn=None
     cur=None
-    pg_host=os.getenv("PG_HOST","localhost")
-    pg_port=os.getenv("PG_PORT","5432")
-    pg_admin_db=os.getenv("PG_ADMIN_DB","postgres")
-    pg_dbname=os.getenv("PG_DBNAME","weather_etl_db")
-    pg_user=os.getenv("PG_USER","postgres")
-    pg_password=os.getenv("PG_PASSWORD","changeme")
+
     try:
-        admin_conn=psycopg2.connect(
-        dbname=pg_admin_db,
-        host=pg_host,
-        user=pg_user,
-        port=pg_port,
-        password=pg_password
-        )
+        admin_conn=database.get_connection(config.PG_ADMIN_DB)
         admin_conn.autocommit=True  
         admin_cur=admin_conn.cursor()
         admin_cur.execute('DROP DATABASE IF EXISTS weather_etl_db')
@@ -39,18 +29,17 @@ def main():
             admin_conn.close()
 
     try:
-        with psycopg2.connect(dbname=pg_dbname,user=pg_user,password=pg_password,host=pg_host,port=pg_port) as conn:
+        with database.get_connection(config.PG_DBNAME) as conn:
             cur=conn.cursor()
-            with open(BASE_DIR/'sql'/'001_schema.sql','r') as file:
-                schema_script=file.read()
+            schema_file=database.execute_sql('001_schema.sql')
+            cur.execute(schema_file)
 
-            cur.execute(schema_script)
+            seed_file=database.execute_sql('002_seed_locations.sql')
+            cur.execute(seed_file)
 
-            with open(BASE_DIR/'sql'/'002_seed_locations.sql','r') as file:
-                location_script=file.read()
-
-            cur.execute(location_script)
             print('Database initialization completed successfully.')
+
+            cur.execute("SELECT COUNT(*) FROM weather.locations")
             
     except Exception as e:
         print(f'Error occurred:{e}')
@@ -58,6 +47,7 @@ def main():
     finally:
         if conn:
             conn.close()
+            
 
 if __name__=='__main__':
     main()
